c3b439d6dbbe771375eb22fe0c78eecff2bf08ae
Dominik Szrek <dszr@pgi.gov.pl>
2022-11-08 10:42:16 +0100

    Dodanie do modelu wyszukiwania dokumentacji kolumny 'txt' - wartości związane z MIDAS'em są rozbudowane

--- search.py
+++ search.py
@@ -12,23 +12,23 @@ from .main import df_from_db
 
 ICON_PATH = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'ui' + os.path.sep
 CASSES = [
-        ['midas_id', '(Numer złoża w bazie MIDAS)', 'rgb(255, 0, 0)'],
-        ['midas_name', '(Nazwa złoża w bazie MIDAS)', 'rgb(180, 0, 0)'],
-        ['cbdg_id', '(Numer dokumentacji w bazie CBDG)', 'rgb(150, 40, 140)'],
-        ['inw_id', '(Archiwalny numer inwentarzowy dokumentacji)', 'rgb(0, 90, 180)'],
-        ['kat_id', '(Archiwalny numer katalogowy dokumentacji)', 'rgb(0, 128, 255)'],
-        ['tag', '(Słowo kluczowe przypisane do dokumentacji)', 'rgb(80, 150, 0)'],
-        ['phrase', '(Fraza wyszukana w tytułach dokumentacji)', 'rgb(100, 100, 100)']
+        ['midas_id', 'Po numerze złoża w bazie MIDAS', 'rgb(255, 0, 0)'],
+        ['midas_name', 'Po nazwie złoża w bazie MIDAS', 'rgb(180, 0, 0)'],
+        ['cbdg_id', 'Po numerze dokumentacji w bazie CBDG', 'rgb(150, 40, 140)'],
+        ['inw_id', 'Po archiwalnym numerze inwentarzowym dokumentacji', 'rgb(0, 90, 180)'],
+        ['kat_id', 'Po archiwalnym numerze katalogowym dokumentacji', 'rgb(0, 128, 255)'],
+        ['tag', 'Po słowie kluczowym przypisanym do dokumentacji', 'rgb(80, 150, 0)'],
+        ['phrase', 'Po frazie wyszukanej w tytułach dokumentacji', 'rgb(100, 100, 100)']
     ]
 RAW_SQLS = [
-            ['midas_id', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d INNER JOIN dokumentacje_midas dm ON d.dok_id = dm.dok_id WHERE dm.midas_id = {val}"],
-            ['midas_name', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d INNER JOIN dokumentacje_midas dm ON d.dok_id = dm.dok_id INNER JOIN midas m ON m.midas_id = dm.midas_id WHERE m.t_zloze_nazwa = '{val}'"],
-            ['cbdg_id', "SELECT dok_id, cbdg_id, t_nr_inw, b_nr_kat, t_dok_tytul, i_dok_rok, t_dok_path FROM dokumentacje d WHERE cbdg_id = {val}"],
-            ['inw_id', "SELECT dok_id, cbdg_id, t_nr_inw, b_nr_kat, t_dok_tytul, i_dok_rok, t_dok_path FROM dokumentacje d WHERE t_nr_inw = '{val}' AND b_nr_kat = false"],
-            ['kat_id', "SELECT dok_id, cbdg_id, t_nr_inw, b_nr_kat, t_dok_tytul, i_dok_rok, t_dok_path FROM dokumentacje d WHERE t_nr_inw = '{val}' AND b_nr_kat = true"],
-            ['tag', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d INNER JOIN dokumentacje_tagi dt ON d.dok_id = dt.dok_id INNER JOIN tagi t ON dt.tag_id = t.tag_id WHERE t.t_tag = '{val}'"],
-            ['phrase', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d WHERE d.t_dok_tytul ilike '%{val}%'"]
-        ]
+        ['midas_id', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d INNER JOIN dokumentacje_midas dm ON d.dok_id = dm.dok_id WHERE dm.midas_id = {val}"],
+        ['midas_name', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d INNER JOIN dokumentacje_midas dm ON d.dok_id = dm.dok_id INNER JOIN midas m ON m.midas_id = dm.midas_id WHERE m.t_zloze_nazwa = '{val}'"],
+        ['cbdg_id', "SELECT dok_id, cbdg_id, t_nr_inw, b_nr_kat, t_dok_tytul, i_dok_rok, t_dok_path FROM dokumentacje d WHERE cbdg_id = {val}"],
+        ['inw_id', "SELECT dok_id, cbdg_id, t_nr_inw, b_nr_kat, t_dok_tytul, i_dok_rok, t_dok_path FROM dokumentacje d WHERE t_nr_inw = '{val}' AND b_nr_kat = false"],
+        ['kat_id', "SELECT dok_id, cbdg_id, t_nr_inw, b_nr_kat, t_dok_tytul, i_dok_rok, t_dok_path FROM dokumentacje d WHERE t_nr_inw = '{val}' AND b_nr_kat = true"],
+        ['tag', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d INNER JOIN dokumentacje_tagi dt ON d.dok_id = dt.dok_id INNER JOIN tagi t ON dt.tag_id = t.tag_id WHERE t.t_tag = '{val}'"],
+        ['phrase', "SELECT d.dok_id, d.cbdg_id, d.t_nr_inw, d.b_nr_kat, d.t_dok_tytul, d.i_dok_rok, d.t_dok_path FROM dokumentacje d WHERE d.t_dok_tytul ilike '%{val}%'"]
+    ]
 
 class DokFromTextSearcher(QFrame):
     """Widget tekstowej wyszukiwarki dokumentacji."""
@@ -86,7 +86,7 @@ class DokFromTextSearcher(QFrame):
             return
         if not self.completer.popup().isVisible() or self.completer.currentIndex().row() == -1:
             # Nie wykorzystano completer'a, wyszukiwanie frazy w tytułach dokumentacji
-            self.act_search = [self.le_search.text(), "phrase"]
+            self.act_search = [self.le_search.text(), self.le_search.text(), "phrase"]
             self.le_search.setText("")
             self.le_search.clearFocus()
 
@@ -94,7 +94,7 @@ class DokFromTextSearcher(QFrame):
         """Ustala parametry wyszukiwania po wyborze elementu z completer'a."""
         self.le_search.setText("")
         self.le_search.clearFocus()
-        self.act_search = [index.data(), index.sibling(index.row(), 1).data()]
+        self.act_search = [index.data(), index.sibling(index.row(), 1).data(), index.sibling(index.row(), 2).data()]
 
     def search_update(self):
         """Aktualizacja po zmianie parametrów wyszukiwania."""
@@ -104,43 +104,60 @@ class DokFromTextSearcher(QFrame):
             return
         self.l_result_title.setText("   Aktualne wyszukiwanie:")
         for case in CASSES:
-            if case[0] == self.act_search[1]:
-                txt = f'<p style="color:{case[2]};"><b>{self.act_search[0]}</b> {case[1]}</p>'
+            if case[0] == self.act_search[2]:
+                txt = f'<p style="color:{case[2]};"><b>{self.act_search[0]}</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="gray"><i>{case[1]}</i></font></p>'
                 self.l_result.setText(txt)
 
     def dataindex_from_db(self):
         """Zwraca dataframe z pobranymi z db indeksami wszystkich dokumentacji."""
         cats = [
-            [f"SELECT DISTINCT midas_id FROM dokumentacje_midas ORDER BY midas_id", 'midas_id'],
-            [f"SELECT DISTINCT ON (m.t_zloze_nazwa) m.t_zloze_nazwa FROM dokumentacje_midas dm INNER JOIN midas m ON dm.midas_id = m.midas_id", 'midas_name'],
+            [f"SELECT DISTINCT ON (m.midas_id) m.midas_id, m.t_zloze_nazwa FROM dokumentacje_midas dm INNER JOIN midas m ON dm.midas_id = m.midas_id", 'midas'],
             [f"SELECT DISTINCT cbdg_id FROM dokumentacje ORDER BY cbdg_id", 'cbdg_id'],
             [f"SELECT DISTINCT t_nr_inw FROM dokumentacje WHERE b_nr_kat = false ORDER BY t_nr_inw", 'inw_id'],
             [f"SELECT DISTINCT t_nr_inw FROM dokumentacje WHERE b_nr_kat = true ORDER BY t_nr_inw", 'kat_id'],
             [f"SELECT DISTINCT t_tag FROM tagi ORDER BY t_tag", 'tag']
         ]
-        df = pd.DataFrame(columns=['val', 'cat'])
+        df = pd.DataFrame(columns=['val', 'txt', 'cat'])
         for cat in cats:
-            tdf = df_from_db(cat[0], ['val'])
-            tdf['val'] = tdf['val'].astype(str)
-            tdf['cat'] = cat[1]
-            df = pd.concat([df, tdf], axis=0)
+            if cat[1] == 'midas':
+                tdf = df_from_db(cat[0], ['id', 'name'])
+                idf = tdf.copy()
+                idf['val'] = idf['id'].astype(str)
+                idf['txt'] = idf.apply(lambda row: f'{row["id"]} ["{row["name"]}"]', axis=1)
+                idf['cat'] = 'midas_id'
+                ndf = tdf.copy()
+                ndf['val'] = ndf['name']
+                ndf['txt'] = ndf.apply(lambda row: f"{row['name']} [{row['id']}]", axis=1)
+                ndf['cat'] = 'midas_name'
+                idf = idf.drop(columns=['id', 'name'])
+                ndf = ndf.drop(columns=['id', 'name'])
+                df = pd.concat([df, idf], axis=0)
+                df = pd.concat([df, ndf], axis=0)
+            else:
+                tdf = df_from_db(cat[0], ['val'])
+                tdf['txt'] = tdf['val']
+                tdf[['val', 'txt']] = tdf[['val', 'txt']].astype(str)
+                tdf['cat'] = cat[1]
+                df = pd.concat([df, tdf], axis=0)
         # Quasi-naturalne sortowanie, bez biblioteki 'natsort':
         df[['_str', '_int']] = df['val'].str.extract(r'([A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]*)(\d*)')
-        df['_str'] = df['_str'].astype('str').str.replace('ł', 'l').str.replace('Ł', 'L').str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+        df['_str'] = df['_str'].astype(str).str.replace('ł', 'l').str.replace('Ł', 'L').str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
         df['_int'] = pd.to_numeric(df['_int'], errors='coerce')
-        df = df.sort_values(by=['_str', '_int'], ignore_index=True).drop(columns=['_str', '_int'])
+        df = df.sort_values(by=['_str', '_int']).drop(columns=['_str', '_int']).reset_index(drop=True)
         return df
 
     def create_index_model(self):
         """W oparciu o dataframe z indeksami dokumentacji (self.df) tworzy model danych do zasilenia QCompleter'a."""
-        model = QStandardItemModel(len(self.df), 2)
+        model = QStandardItemModel(len(self.df), 3)
         for index in self.df.to_records():
-            item_1 = QStandardItem(index[1])
-            item_1.setIcon(QIcon(f"{ICON_PATH}{index[2]}.png"))
+            item_1 = QStandardItem(index[2])
+            item_1.setIcon(QIcon(f"{ICON_PATH}{index[3]}.png"))
             item_1.setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
-            item_2 = QStandardItem(index[2])
+            item_2 = QStandardItem(index[1])
+            item_3 = QStandardItem(index[3])
             model.setItem(index[0], 0, item_1)
             model.setItem(index[0], 1, item_2)
+            model.setItem(index[0], 2, item_3)
         return model
 
     def df_from_dok_search(self):
@@ -154,9 +171,9 @@ class DokFromTextSearcher(QFrame):
     def sql_parser(self):
         """Zwraca tekst sql z wartością pobraną z 'self.act_search'."""
         sql = None
-        val = self.act_search[0]
+        val = self.act_search[1]
         for raw_sql in RAW_SQLS:
-            if raw_sql[0] == self.act_search[1]:
+            if raw_sql[0] == self.act_search[2]:
                 sql = eval('f"{}"'.format(raw_sql[1]))
                 break
         return sql
